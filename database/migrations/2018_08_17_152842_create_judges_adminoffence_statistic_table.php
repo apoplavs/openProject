b1e4981fe297bb0b8a1273097f1362e684f6698a
apoplavs <p.andriy.v@gmail.com>
2018-08-21 13:37:34 +0300

    made migrations

---
+++ database/migrations/2018_08_17_152842_create_judges_adminoffence_statistic_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJudgesAdminoffenceStatisticTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('judges_adminoffence_statistic', function (Blueprint $table) {
+            $table->integer('judge')->unique();
+            $table->smallInteger('amount')->nullable()->comment('кількість розглянутих справ в порядку КУпАП');
+            $table->smallInteger('cases_on_time')->nullable()->comment('кількість справ розглянутих в визначені законом строки');
+            $table->smallInteger('average_duration')->nullable()->comment('середня тривалість розгляду справи');
+            $table->smallInteger('positive_judgment')->nullable()->comment('кількість постанов в яких особу звільнено від відповідальності');
+            $table->smallInteger('negative_judgment')->nullable()->comment('кількість постанов в яких особу притягнено до відповідальності');
+            $table->smallInteger('was_appeal')->nullable()->comment('кількість постанов на які подавалась апеляція');
+            $table->smallInteger('approved_by_appeal')->nullable()->comment('кількість постанов які встояли в апеляції');
+            $table->smallInteger('not_approved_by_appeal')->nullable()->comment('кількість постанов які не встояли в апеляції');
+
+            $table->foreign('judge')->references('id')->on('judges');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('judges_adminoffence_statistic');
+    }
+}
