98713dc393f15ef67d127def5881f51a994565b1
slualexvas <slualexvas@gmail.com>
2018-10-31 00:50:16 +0200

    created JudgeListTest and JudgeListGuestTest

---
+++ tests/Feature/JudgesListTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Toecyd\Judge;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder;
+
+class JudgesListTest extends JudgesListGuestTest
+{
+    public function setUp() {
+        parent::setUp();
+
+        $this->url = str_replace('guest/', '', $this->url);
+    }
+
+    protected function getFieldstoSelect()
+    {
+        return Judge::getJudgesListFields($this->user->id);
+    }
+
+    public function getJudgesQuery(bool $powers_expired = false, array $orderBy = ['judges.surname', 'ASC']) : Builder
+    {
+        return parent::getJudgesQuery($powers_expired, $orderBy)
+            ->leftJoin('user_bookmark_judges', function ($join){
+                $join->on('judges.id', '=', 'user_bookmark_judges.judge');
+                $join->on('user_bookmark_judges.user', '=', DB::raw($this->user->id));
+            });
+    }
+}
