f3806e047664b2502b8f54c0df9b359dc98db20f
Oleksandr Baranov <oleksandr.baranov@smashdocs.net>
2018-11-04 16:38:38 +0200

    Few changes in db.py

--- ml/lib/db.py
+++ ml/lib/db.py
@@ -1,28 +1,31 @@
 import pymysql.cursors
 from lib.config import *
 
+edrsr_connection = pymysql.connect(host=DB_EDRSR['host'],
+                                   user=DB_EDRSR['user'],
+                                   password=DB_EDRSR['pass'],
+                                   db=DB_EDRSR['dbname'],
+                                   charset=DB_EDRSR['charset'],
+                                   cursorclass=pymysql.cursors.DictCursor)
+
+toecyd_connection = pymysql.connect(host=DB_TOECYD['host'],
+                                    user=DB_TOECYD['user'],
+                                    password=DB_TOECYD['pass'],
+                                    db=DB_TOECYD['dbname'],
+                                    charset=DB_TOECYD['charset'],
+                                    cursorclass=pymysql.cursors.DictCursor)
+
 
 class DB:
-
     def __init__(self, db_name):
 
-        if not db_name in (EDRSR, TOECYD):
+        if db_name not in (EDRSR, TOECYD):
             raise Exception('Please provide valid db_name')
 
         if db_name == EDRSR:
-            self.connection = pymysql.connect(host=DB_EDRSR['host'],
-                                              user=DB_EDRSR['user'],
-                                              password=DB_EDRSR['pass'],
-                                              db=DB_EDRSR['dbname'],
-                                              charset=DB_EDRSR['charset'],
-                                              cursorclass=pymysql.cursors.DictCursor)
+            self.connection = edrsr_connection
         elif db_name == TOECYD:
-            self.connection = pymysql.connect(host=DB_TOECYD['host'],
-                                              user=DB_TOECYD['user'],
-                                              password=DB_TOECYD['pass'],
-                                              db=DB_TOECYD['dbname'],
-                                              charset=DB_TOECYD['charset'],
-                                              cursorclass=pymysql.cursors.DictCursor)
+            self.connection = toecyd_connection
 
     def write(self, sql_query, values):
 
@@ -32,7 +35,6 @@ class DB:
         self.connection.commit()
 
     def read(self, sql_query):
-        #print(sql_query)
         with self.connection.cursor() as cursor:
             cursor.execute(sql_query)
             result = cursor.fetchall()
