a146a190c49c3f0d1dfa2cf6d7efb97ec10e81b2
Tatiana Prysiazhnyuk <tprysiaz@student.unit.ua>
2018-11-24 17:43:53 +0200

    saved

--- resources/assets/js/components/rating/judges/JudgeProfile.vue
+++ resources/assets/js/components/rating/judges/JudgeProfile.vue
@@ -215,43 +215,12 @@
                 </div>
             </div>
         </div>
-        <!-- modal -->
-        <modal v-show="isModalVisible" @close="closeModal" @save="saveChanges">
-            <h4 slot="header">Оновити статус судді</h4>
-            <div slot="body">
-                <form>
-                    <div class="form-group row mx-0 my-4">
-                        <label for="chooser-judge-status" class="col-4">Статус</label>
-                        <div class="col-8">
-                            <select class="form-control" id="chooser-judge-status">
-                                        <option value="1">на роботі</option>
-                                        <option value="2">на лікарняному</option>
-                                        <option value="3">у відпустці</option>
-                                        <option value="4">відсутній на робочому місці</option>
-                                        <option value="5">припинено повноваження</option>
-                                    </select>
-                        </div>
-                        <input type="hidden" id="judge-for-new-status" value="0">
-                    </div>
-                    <div class="form-group row mx-0 my-4">
-                        <label for="status-end-date" class="col-7">Дата завершення дії статусу <br><sup class="text-muted">(якщо відома)</sup></label>
-                        <div class="col-5">
-                            <!-- <datepicker v-model="judgeStatus.due_date" :value="judgeStatus.due_date" language="uk" :min="calendar.startDate | formatDate" :max="calendar.endDate | formatDate"> -->
-                            <!-- </datepicker> -->
-                        </div>
-                    </div>
-                </form>
-            </div>
-    
-        </modal>
     </div>
 </template>
 
 <script>
     import Spinner from "../../shared/Spinner.vue";
-    import {
-        GChart
-    } from "vue-google-charts";
+    import GChart from "vue-google-charts";
     import DoughnutChart from 'vue-doughnut-chart'
 
     
