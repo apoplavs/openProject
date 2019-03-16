5655eca61242f000b438627134098b27141719b3
apoplavs <p.andriy.v@gmail.com>
2019-03-16 13:22:58 +0200

    add abaranov to team

--- resources/assets/js/components/main/AboutComponent.vue
+++ resources/assets/js/components/main/AboutComponent.vue
@@ -69,13 +69,13 @@
                 <ul class="row">
                     <li class="col-12 col-md-6 col-lg-3">
                         <div class="cnt-block equal-hight" style="height: 349px;">
-                            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+                            <figure><img src="../../../images/abaranov.jpg" class="img-responsive" alt=""></figure>
                             <h3>Баранов Олександр</h3>
                             <p>Data Scientist</p>
                             <ul class="follow-us clearfix">
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.facebook.com/photo.php?fbid=1006014652888615&set=a.256940597796028&type=3&theater" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <!-- <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li> -->
+                                <li><a href="https://www.linkedin.com/in/barrannov/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                             </ul>
                         </div>
                     </li>
@@ -94,11 +94,11 @@
                     </li>
                     <li class="col-12 col-md-6 col-lg-3">
                         <div class="cnt-block equal-hight" style="height: 349px;">
-                            <figure><img src="../../../images/tprysiazh.jpeg" class="img-responsive" alt=""></figure>
+                            <figure><img src="../../../images/tprysiazh.jpg" class="img-responsive" alt=""></figure>
                             <h3>Тетяна Присяжнюк</h3>
                             <p>Front End Developer</p>
                             <ul class="follow-us clearfix">
-                                <li><a href="#" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="https://www.facebook.com/profile.php?id=100010596070139" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                                 <li><a href="https://github.com/Tanyuta" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
                                 <li><a href="https://www.linkedin.com/in/tanyuta/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                             </ul>
@@ -107,7 +107,8 @@
                     <li class="col-12 col-md-6 col-lg-3">
                         <div class="cnt-block equal-hight" style="height: 349px;">
                             <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt=""></figure>
-                            <h3>Слуцький Олександр</h3>
+                            <h3>Mr. Ingnito</h3>
+                            <!-- <h3>Слуцький Олександр</h3> -->
                             <p>Back End Developer</p>
                             <ul class="follow-us clearfix">
                                 <li><a href="#" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
